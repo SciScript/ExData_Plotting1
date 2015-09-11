36752f50f2ab886992da8f4408973c1138cd9fa6
SciScript <SciScript@gmail.com>
2015-09-11 06:05:45 -0700

    Update plot2.r

--- plot2.r
+++ plot2.r
@@ -1,3 +1,5 @@
+library(lubridate)
+
 # Read in the data and filter the dates
 dat <- data.table::fread("household_power_consumption.txt")
 dat1 <- filter(dat, Date == "1/2/2007" | Date == "2/2/2007")
@@ -10,7 +12,8 @@ dat1$Global_active_power <- as.numeric(dat1$Global_active_power)
 dat1$Date_Time <- paste(dat1$Date, dat1$Time, sep = " ")
 dat1$Date_Time <- dmy_hms(dat1$Date_Time)
 
-# Create plot2
+# Create plot2 (ATTENTION: my computer is in German, therefore the labels appear in German as
+# Do, Fr Sa (= Thu, Fri, Sat))
 png(filename="plot2.png", width = 480, height = 480)
 with(dat1, plot(Date_Time, Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = ""))
 dev.off()
