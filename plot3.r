0d1b51cf120666b1ef3644d3b88a84bde3fb03a2
SciScript <SciScript@gmail.com>
2015-09-11 06:07:44 -0700

    Update plot3.r

--- plot3.r
+++ plot3.r
@@ -1,3 +1,5 @@
+library(lubridate)
+
 # Read in the data and filter the dates
 dat <- data.table::fread("household_power_consumption.txt")
 dat1 <- filter(dat, Date == "1/2/2007" | Date == "2/2/2007")
@@ -10,7 +12,8 @@ dat1$Global_active_power <- as.numeric(dat1$Global_active_power)
 dat1$Date_Time <- paste(dat1$Date, dat1$Time, sep = " ")
 dat1$Date_Time <- dmy_hms(dat1$Date_Time)
 
-# Create plot3
+# Create plot3 (ATTENTION: my computer is in German, therefore the labels appear in German as
+# Do, Fr Sa (= Thu, Fri, Sat))
 png(filename="plot3.png", width = 480, height = 480)
 plot(dat1$Date_Time, dat1$Sub_metering_1, type = "n", xlab="", ylab="Energy sub metering")
 lines(dat1$Date_Time, dat1$Sub_metering_1, col = "black")
